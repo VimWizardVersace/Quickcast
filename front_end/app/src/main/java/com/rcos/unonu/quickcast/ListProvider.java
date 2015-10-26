8f36fa35a927d1ba0517c5dc730d9eb46a312606
unonu <unonuorea@gmail.com>
2015-10-25 22:43:48 -0400

    Added the basic main activity and some stuff for the Provider (read Adapter). This shouldn't build, but I wanted to upload something while waiting for the main_list_element layout to be finished.

---
+++ front_end/app/src/main/java/com/rcos/unonu/quickcast/ListProvider.java
@@ -0,0 +1,104 @@
+package com.rcos.unonu.quickcast;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by unonu on 10/25/15.
+ */
+
+/*
+	Okay here:
+	Get Recent Matches —>
+	{ match ID :
+		{ "start time",
+			"sport",
+			"teams" : [ team ID 1, team ID 2],
+			"score" : [hero score 1, hero score 2],
+			"series" : [ games won 1, games won 2, series length]
+		},
+		...
+	}
+ */
+
+public class ListElement {
+	public ListElement(String id, JSONObject details) throws JSONException {
+		matchID = id;
+
+		JSONArray teams = details.getJSONArray("teams");
+		JSONArray scores = details.getJSONArray("score");
+		JSONArray jsonSeries = details.getJSONArray("series");
+
+		sport		= details.getString("sport");
+		timestamp	= details.getInt("start time");
+		endStamp	= details.optInt("end time", -1);
+		series		= new int [3];
+		series[0]	= jsonSeries.getInt(0);
+		series[1]	= jsonSeries.getInt(1);
+		series[2]	= jsonSeries.getInt(2);
+		score1		= scores.getInt(0);
+		score2		= scores.getInt(1);
+		team1		= teams.getString(0);
+		teamID1		= teams.getString(1);
+		team2		= teams.getString(2);
+		teamID2		= teams.getString(3);
+
+	}
+
+	int timestamp;
+	int endStamp;
+	int score1;
+	int score2;
+	int [] series;
+	String matchID;
+	String sport;
+	String team1;
+	String teamID1;
+	String team2;
+	String teamID2;
+
+}
+
+public class ListProvider extends ArrayAdapter<ListElement> {
+	private Context context;
+	private ArrayList<ListElement> elements;
+
+	public ListProvider(Context context) {
+		super(context, -1);
+		this.elements = new ArrayList<ListElement>();
+		this.context = context;
+	}
+
+	public int setElements(String blob) throws JSONException {
+		this.elements = new ArrayList<ListElement>();
+		JSONObject matches = new JSONObject(blob);
+
+		Iterator<String> ids = matches.keys();
+		while (ids.hasNext()) {
+			String id = ids.next();
+			this.elements.add(new ListElement(id, matches.getJSONObject(id)));
+		}
+		return this.elements.size();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		View element = inflater.inflate(R.layout.main_list_element, parent, false);
+
+		/* View elements to inject */
+
+		return element;
+	}
+}
