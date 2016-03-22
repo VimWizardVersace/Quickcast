e4937bc65a50df522168cab621ba1ea5acd21882
Farkinator <thefarkinator@gmail.com>
2016-03-21 22:56:50 -0400

    Generalized /live

--- back_end/quickAPI/routes/index.js
+++ back_end/quickAPI/routes/index.js
@@ -15,9 +15,34 @@ router.get('/', function(req, res, next) {
 
 router.get('/live', function(req, res, next){
 	//Note: There should only ever be one livegame object. Select only csgogame, lolgames, and dotagames.
-	Live.findOne({}, {csgogames : 1, lolgames : 1, dotagames : 1, _id : 0}, function(err, live){
+	Live.findOne({}, {csgogames : 1, lolgames : 1, dotagames : 1, _id : 0}, function(err, livegames){
 		if(err) return next(err);
-		res.json(live);
+		var simpleresponse = [];
+		//This is how we should traverse
+		for(var i=0; i<livegames.dotagames.length; i++){
+			var currentgame = livegames.dotagames[i];
+			if(currentgame.scoreboard != null && currentgame.dire_team != null && currentgame.radiant_team != null){
+				var totalgames;
+				//Series type 0 = best of 1, and so on.
+				if(currentgame.series_type = 0){
+					totalgames = 1;
+				} else if (currentgame.series_type = 1){
+					totalgames = 3;
+				} else if (currentgame.series_type = 2){
+					totalgames = 5;
+				}
+				//See the help document on my blog for demystifying the dota 2 web api.
+				simpleresponse.push({
+					"matchid" : currentgame.match_id, 
+					"sport" : "DOTA2",
+					"series" : [currentgame.dire_series_wins, currentgame.radiant_series_wins,totalgames],
+					"score" : [currentgame.scoreboard.dire.score, currentgame.scoreboard.radiant.score],
+					"teams" : [currentgame.dire_team.team_name, currentgame.radiant_team.team_name]
+				});
+			}
+		}
+
+		res.json(simpleresponse);
 	});
 });
 module.exports = router;
