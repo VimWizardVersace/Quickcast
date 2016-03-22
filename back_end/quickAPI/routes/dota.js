100f8c6f2e0148da4033900752a2bf0351b6d809
Farkinator <thefarkinator@gmail.com>
2016-03-21 22:46:27 -0400

    live should now provide simplified info

--- back_end/quickAPI/routes/dota.js
+++ back_end/quickAPI/routes/dota.js
@@ -15,7 +15,28 @@ router.get('/live', function(req, res, next){
 	Live.find(function(err, livegames){
 		if(err) return next(err);
 		//There should ONLY ever be one live game object.
-		res.json(livegames[0].dotagames);
+		var simpleresponse = [];
+		for(var i=0; i<livegames[0].dotagames.length; i++){
+			var currentgame = livegames[0].dotagames[i];
+			if(currentgame.scoreboard != null && currentgame.dire_team != null && currentgame.radiant_team != null){
+				var totalgames;
+				if(currentgame.series_type = 0){
+					totalgames = 1;
+				} else if (currentgame.series_type = 1){
+					totalgames = 3;
+				} else if (currentgame.series_type = 2){
+					totalgames = 5;
+				}
+				simpleresponse.push({
+					"matchid" : currentgame.match_id, 
+					"series" : [currentgame.dire_series_wins, currentgame.radiant_series_wins,totalgames],
+					"score" : [currentgame.scoreboard.dire.score, currentgame.scoreboard.radiant.score],
+					"teams" : [currentgame.dire_team.team_name, currentgame.radiant_team.team_name]
+				});
+			}
+		}
+
+		res.json(simpleresponse);
 	});
 });
 router.get('/live/:id', function(req, res, next){
